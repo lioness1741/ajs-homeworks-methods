5bcb6b20db7bb7503e3e5718da872d844b5ea309
Tatyana <lioness1741@mail.ru>
2023-09-14 16:48:03 +0600

    fixed4

--- src/js/character.js
+++ src/js/character.js
@@ -1,3 +1,38 @@
+export default class Character {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;   
+    this.health = 100;
+    this.level = 1;
+    this.attack = 10;
+    this.defence = 10;
+  }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error("нельзя повысить левел умершего");
+    }
+
+    this.level++;
+    this.attack = Math.round(this.attack * 1.2);
+    this.defence = Math.round(this.defence * 1.2);
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health === 0) {
+      throw new Error("нельзя повысить левел умершего");
+    }
+
+    this.health -= points * (1 - this.defence / 100);
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
+}
+
+
+/*
 export const params = {
     Bowman: {
       attack: 25,
@@ -85,3 +120,4 @@ export const params = {
       this.health -= points * (1 - this.defence / 100);
     }
   }
+*/
